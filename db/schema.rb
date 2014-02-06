baf9845af939cd7411836f96b15127d2efe0f878
soramugi <soramugi.chika@gmail.com>
2014-02-06 09:38:48 +0900

    db update

--- db/schema.rb
+++ db/schema.rb
@@ -13,15 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140205160426) do
 
-  create_table "images", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-  end
-
   create_table "items", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"

bfbc5277caf1a1f7d660eb43d9bbdf2642dc7571
soramugi <soramugi.chika@gmail.com>
2014-05-12 12:36:10 +0900

    bundle update

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
-  has_attached_file :image, styles: {thumb: '300x300#'}
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :image, styles: { thumb: '300x300#' }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   default_scope { order('id DESC') }
 
   def self.create_with_imagedata data
